90001928cd44f32260def1b2520822cf40f19ffc
NisseBoman <nisse@tnfgruppen.com>
2023-11-22 14:55:28 +0100

    Update index.js
    Changed method to fetch images to be able to override cache-policy but also strip out any headers from the source.
    
    also changed some format of my console.log statements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import { env } from "fastly:env";
 import { allowDynamicBackends } from "fastly:experimental";
+import { CacheOverride } from "fastly:cache-override";
 
 allowDynamicBackends(true);
 
@@ -22,11 +23,22 @@ async function handleRequest(event) {
       status: 405,
     });
   }
+  console.log("FASTLY_HOSTNAME:", env("FASTLY_HOSTNAME"));
 
   // Create new URL object
   let url = new URL(req.url);
+  
+  // Verify it's not local testing and also verify a token from the CDN service to avoid miss use of the service
+  if(env("FASTLY_HOSTNAME") != "localhost") {
+    if (req.headers.get("Token") != "948390khbams3") //Please note that this is a secret and should be changed
+    {
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    }
+  }
 
-  console.log("request url: " + req.url.toString());
+  console.log("request url:", req.url.toString());
 
   // spara undan QS och lägg till i svaret tillbaka till delivery 
   let searchEntries = url.search;
@@ -34,7 +46,7 @@ async function handleRequest(event) {
   // destination path. 
   let dest = url.pathname.toString();
   
-  console.log("Destination: " + dest);
+  console.log("Destination: ", dest);
 
   let tmpExt = dest.split(".");
   tmpExt = tmpExt[tmpExt.length -1].toLowerCase();
@@ -46,10 +58,33 @@ async function handleRequest(event) {
   {
     
     let backendURL = "https:/" + dest + searchEntries; // Note that dest contains a leading /
-    console.log("backendURL: " + backendURL);
+    console.log("backendURL: ", backendURL);
     
-    return fetch(backendURL);
+    
+    const myHeaders = new Headers();
+   
+    myHeaders.append('Accept-Encoding','gzip, deflate, br');
+    myHeaders.append('Accept', 'image/*');
+    //myHeaders.append('user-agent', 'curl/7.21.0 (x86_64-pc-linux-gnu) libcurl/7.22.0 OpenSSL/1.0.1 zlib/1.2.3.4 libidn/1.23 librtmp/2.3');
+    myHeaders.append('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36');
 
+    // Set the TTL for the source image to 120 secs and override any other cache settings. Cache can be purged in 150ms!
+    let cacheOverride = new CacheOverride('override', {ttl: 120}); 
+
+    const response = await fetch(backendURL, {
+      headers: myHeaders,
+      method: "GET",
+      cacheOverride
+    }); 
+
+    return new Response(response.body, {
+      status: 200,
+      headers : new Headers({
+        "Content-Type": response.headers.get("Content-Type"), 
+        "Accept-Ranges": response.headers.get("Accept-Ranges")   
+      })
+    });
+    // End new stuff 
   
   }else{
     return new Response("Error - No or incorrect image specified in path", {status:400});
